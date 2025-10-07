cde66c70d6b59aa3d7a8736fad1b9b5ea5ac9532
Dhritiraj-Deka <dhritirajd11@gmail.com>
2025-10-07 21:09:04 +0530

    Uploaded Blackjack
    I was taught multiple things, but I wanted to bring all of them together in a simple program.

---
+++ Blackjack.c
@@ -0,0 +1,40 @@
+#include <stdio.h> 
+#include <stdlib.h>
+#include <time.h>
+
+
+int main() 
+{
+//PART 1- DEFINING
+    srand(time(0));
+    int Score_PL,N,Score_PC,C;
+    Score_PC = (rand()%(5))+17;
+    Score_PL = (rand()%11)+1;
+do {printf("Your score is %d. Do you want another card? (1 for Yes, 0 for No) ",Score_PL);
+    scanf("%d",&N);
+    if (N==1)
+    {
+        Score_PL = Score_PL + (rand()%11)+1;
+    }
+} while (N==1 && Score_PL<21);
+
+
+printf("\nThe House scored %d \n",Score_PC);
+
+//PART 2- FINAL RESULT
+if (Score_PL<21)
+    {
+        if(Score_PC<Score_PL)
+            {
+                if(Score_PL != Score_PC){printf("WIN!");}
+                else{printf("TIE!");}
+            }
+        else{printf("LOSE!");}
+    }
+else {printf("BUST!");}
+    
+
+//PART 4- END
+    getchar();
+    return 0;
+} 
